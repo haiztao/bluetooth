2f933a580dd2de8d9884c111fe01441544cc0593
haitao <haiztao@163.com>
2016-07-21 10:51:30 +0800

    Initial Commit

---
+++ 健康卫士Tests/____Tests.swift
@@ -0,0 +1,36 @@
+//
+//  ____Tests.swift
+//  健康卫士Tests
+//
+//  Created by haitao on 16/7/21.
+//  Copyright © 2016年 haitao. All rights reserved.
+//
+
+import XCTest
+@testable import ____
+
+class ____Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
