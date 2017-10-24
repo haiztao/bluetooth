347e2413eb41dddf02b50d9b721b84e99076be17
haiztao <zhanghaitao@haitaodeMacBook-Pro.local>
2017-10-24 16:10:43 +0800

    change
    c

---
+++ bluetooth/bluetooth/BLEManage.swift
@@ -0,0 +1,18 @@
+//
+//  BLEManage.swift
+//  bluetooth
+//
+//  Created by haitao on 2017/10/24.
+//  Copyright © 2017年 haitao. All rights reserved.
+//
+
+import UIKit
+import CoreBluetooth
+
+class BLEManage: NSObject {
+
+    var bleManager : CBCentralManager!
+    
+    
+    
+}
